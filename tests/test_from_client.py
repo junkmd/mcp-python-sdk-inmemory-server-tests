981c93fba0eb5ae49d1e2c2f2f857ca3a87e4cef
Jun Komoda <45822440+junkmd@users.noreply.github.com>
2025-04-26 01:04:06 +0000

    Adjust indentation styles.

--- tests/test_from_client.py
+++ tests/test_from_client.py
@@ -25,32 +25,32 @@ class Test_FromClient:
     async def test_greet_asyncio_run(self):
         async with create_client() as client:
             res = await client.call_tool("greet_asyncio_run", {"name": "you"})
-            cnt = res.content[0]
-            assert isinstance(cnt, TextContent)
-            assert cnt.text != "Hello, you!"  # CHECK THIS OUT!
-            assert ASYNCIO_EVENT_LOOP_ERR_MSG in cnt.text
-            assert res.isError  # CHECK THIS OUT!
+        cnt = res.content[0]
+        assert isinstance(cnt, TextContent)
+        assert cnt.text != "Hello, you!"  # CHECK THIS OUT!
+        assert ASYNCIO_EVENT_LOOP_ERR_MSG in cnt.text
+        assert res.isError  # CHECK THIS OUT!
 
     @pytest.mark.asyncio
     async def test_greet(self):
         async with create_client() as client:
             res = await client.call_tool("greet", {"name": "you"})
-            cnt = res.content[0]
-            assert isinstance(cnt, TextContent)
-            assert cnt.text == "Hello, you!"
+        cnt = res.content[0]
+        assert isinstance(cnt, TextContent)
+        assert cnt.text == "Hello, you!"
 
     @pytest.mark.asyncio
     async def test_add(self):
         async with create_client() as client:
             res = await client.call_tool("add", {"a": 1, "b": 2})
-            cnt = res.content[0]
-            assert isinstance(cnt, TextContent)
-            assert cnt.text == "3"
+        cnt = res.content[0]
+        assert isinstance(cnt, TextContent)
+        assert cnt.text == "3"
 
     @pytest.mark.asyncio
     async def test_multiply(self):
         async with create_client() as client:
             res = await client.call_tool("multiply", {"a": 2, "b": 3})
-            cnt = res.content[0]
-            assert isinstance(cnt, TextContent)
-            assert cnt.text == "6"
+        cnt = res.content[0]
+        assert isinstance(cnt, TextContent)
+        assert cnt.text == "6"
